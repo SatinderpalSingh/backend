651ecfed945908cc3d607d43b1d7d77e3e552ddd
lakshaychopra <lakshaychopra21@gmail.com>
2019-07-18 00:16:13 +0530

    resolved migration error

--- Mission/models.py
+++ Mission/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class mission(models.Model):
+    Mission = models.TextField(max_length = 1000)
+
+    def __str__(self):
+        return self.Mission
+
+
+
+
