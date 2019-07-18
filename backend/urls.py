eadfb039adfa9f05e969557ca73ddf36e4656c03
lakshaychopra <lakshaychopra21@gmail.com>
2019-07-18 10:55:45 +0530

    removed urls of Mission and Vission

--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+'''
 urlpatterns = [
+    path('website/',include('website.urls')),
     path('vision/',include('Vision.urls')),
     path('mission/',include('Mission.urls')),
     path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+'''
+
+urlpatterns = [
+    path('website/',include('website.urls')),
     path('admin/', admin.site.urls),
 ]
